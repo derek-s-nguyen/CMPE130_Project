d13bd1775f34d5aec29f95f851be3f06f942c084
Derek Nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-04-29 21:12:07 -0700

    implementing Jobs.hpp updated functions

--- Jobs.cpp
+++ Jobs.cpp
@@ -4,19 +4,26 @@
 #include <fstream>
 using namespace std;
 #include <iostream>
-Jobs::Jobs(){ arrival = 0, completion = 0, ID = 0, duration = 0, startTime = 0, finishTime = 0, totalTimeElapsed = 0, responseTime = 0, remainingTime = 0; }
+Jobs::Jobs(){ arrival = 0, completion = 0, ID = 0, duration = 0, startTime = 0, finishTime = 0, totalTimeElapsed = 0, responseTime = 0, remainingTime = 0, jobStarted = false; }
 int Jobs::getArrival(){ return arrival; }
 int Jobs::getID(){ return ID; }
 int Jobs::getDuration(){ return duration; }
 int Jobs::getRemainingTime(){ return remainingTime; }
 void Jobs::takeTimeSliceAway(int timeToTakeAway){
-	remainingTime = remainingTime - timeToTakeAway;//we chose 5 as the quanta of time that a job is alloted in the RR scheduler
+	remainingTime = remainingTime - timeToTakeAway;//we chose 1 as the quanta of time that a job is alloted in the RR scheduler
 }
 void Jobs::operator =(const Jobs &job)
 {
 	ID = job.ID;
 	arrival = job.arrival;
 	duration = job.duration;
+	startTime = job.startTime;
+	finishTime = job.finishTime;
+	remainingTime = job.remainingTime;
+	responseTime = job.responseTime;
+	completion = job.completion;
+	totalTimeElapsed = job.totalTimeElapsed;
+	jobStarted = job.jobStarted;
 }
 istream &operator >>(istream &in_stream, Jobs &job){
 	in_stream >> job.ID;
@@ -34,7 +41,10 @@ int Jobs::getStartTime(){ return startTime; }
 int Jobs::getFinishTime(){ return finishTime; }
 int Jobs::getTotalTimeElapsed(){ return totalTimeElapsed; }
 int Jobs::getResponseTime(){ return responseTime; }
+bool Jobs::getjobStarted(){ return jobStarted; }
 void Jobs::setStartTime(int start){ startTime = start; }
 void Jobs::setFinishTime(int finish){ finishTime = finish; }
 void Jobs::setTotalTimeElapsed(int total){ totalTimeElapsed = total; }
 void Jobs::setResponseTime(int response){ responseTime = response; }
+void Jobs::setRemainingTime(int time){ remainingTime = time; }
+void Jobs::setjobStarted(bool started){ jobStarted = started; }
